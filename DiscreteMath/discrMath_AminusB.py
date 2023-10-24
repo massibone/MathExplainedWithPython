de420a36f9c2dd10e7289456f1109ac34df320ab
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-24 08:05:53 +0200

    Add files via upload

---
+++ DiscreteMath/discrMath_AminusB.py
@@ -0,0 +1,13 @@
+'''
+differenza tra due insiemi A e B
+'''
+
+# Definisci gli insiemi A e B
+A = {4, 1, 7}
+B = {1, 7, 3}
+
+# Calcola la differenza tra A e B
+differenza = A - B
+
+# Stampa il risultato
+print("La differenza tra A e B è:", differenza)
