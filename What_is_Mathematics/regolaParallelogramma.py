ea102d0eda03b6dd43801c2e500e2d4e6ad71ec1
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-04 08:22:35 +0100

    Update regolaParallelogramma.py

--- What_is_Mathematics/regolaParallelogramma.py
+++ What_is_Mathematics/regolaParallelogramma.py
@@ -1,1 +1,3 @@
-
+'''
+La regola del parallelogramma è una rappresentazione geometrica utilizzata per l'addizione di numeri complessi. Essa mostra come sommare due numeri complessi utilizzando il concetto di vettori in un piano complesso.
+'''
