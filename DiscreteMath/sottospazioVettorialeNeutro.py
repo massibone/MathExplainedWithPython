c1aba5c54a43a7e74f38a8f41a2db3695c049afe
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-05 01:19:19 +0100

    Update sottospazioVettorialeNeutro.py

--- DiscreteMath/sottospazioVettorialeNeutro.py
+++ DiscreteMath/sottospazioVettorialeNeutro.py
@@ -1,1 +1,11 @@
 
+import numpy as np
+
+# Matrice neutra (elemento neutro rispetto alla somma)
+elemento_neutro = np.zeros((3, 3))
+
+# Verifica che la matrice neutra appartenga al sottospazio
+# S, cioè che la prima riga sia uguale a zero
+elemento_neutro_appartiene_a_S = np.all(elemento_neutro[0] == 0)
+
+print("L'elemento neutro appartiene a S?", elemento_neutro_appartiene_a_S)
