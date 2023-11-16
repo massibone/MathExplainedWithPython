8336636dc14b6a08c4ac8e180704fca7a3af49a9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-16 09:47:49 +0100

    Update regolaParallelogramma.py

--- What_is_Mathematics/regolaParallelogramma.py
+++ What_is_Mathematics/regolaParallelogramma.py
@@ -6,3 +6,19 @@ def somma_complessi(z1, z2):
     parte_reale_z1, parte_immaginaria_z1 = z1
     parte_reale_z2, parte_immaginaria_z2 = z2
   
+  # Calcolare la somma delle parti reali e delle parti immaginarie
+    somma_reale = parte_reale_z1 + parte_reale_z2
+    somma_immaginaria = parte_immaginaria_z1 + parte_immaginaria_z2
+    
+    # Restituire la somma come un numero complesso
+    return (somma_reale, somma_immaginaria)
+
+# Numeri complessi in forma (parte reale, parte immaginaria)
+z1 = (2, 3)  # 2 + 3i
+z2 = (1, -1)  # 1 - i
+
+# Calcolare la somma dei numeri complessi
+risultato = somma_complessi(z1, z2)
+
+# Stampare il risultato
+print("La somma dei numeri complessi è:", f"{risultato[0]} + {risultato[1]}i")
