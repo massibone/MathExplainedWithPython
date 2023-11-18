3580b6370e9fc37e9804c73c361959390aa5741c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-19 00:47:47 +0100

    Update scomposizioneinfattori.py

--- What_is_Mathematics/scomposizioneinfattori.py
+++ What_is_Mathematics/scomposizioneinfattori.py
@@ -1,1 +1,14 @@
 
+import sympy as sp
+
+# Definizione della variabile simbolica
+x = sp.symbols('x')
+
+# Espressione da scomporre
+espressione = x**4 - 1
+
+# Scomposizione in fattori
+fattori = sp.factor(espressione)
+
+print("Scomposizione in fattori:")
+print(fattori)
