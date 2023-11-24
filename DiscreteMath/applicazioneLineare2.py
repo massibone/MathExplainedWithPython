d200e2a35f27566c525a69807ab174cdbcb6d95e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-24 11:29:33 +0100

    Create applicazioneLineare2.py

---
+++ DiscreteMath/applicazioneLineare2.py
@@ -0,0 +1,25 @@
+'''
+Si consideri l’applicazione lineare f : M(3; R) → M(3; R) tale che
+f (1; 1; 0) = (3; 0; −1)
+f (1; 0; 1) = (1; 1; −2)
+f (0; 1; 1) = (0; 1; 0):
+Allora la matrice A ∈ M(3; 3; R) associata alla base canonica tale che f = fA è
+
+2 1 −1
+0 0 1
+-3/2 1/2 -1/2
+
+'''
+
+import numpy as np
+
+# Definire i vettori immagine f(e_i)
+f_e1 = np.array([2, 1, -1])
+f_e2 = np.array([0, 0, 1])
+f_e3 = np.array([-3/2, 1/2, -1/2])
+
+# Organizzare i vettori come colonne della matrice A
+A = np.column_stack((f_e1, f_e2, f_e3))
+
+print("La matrice A è:")
+print(A)
