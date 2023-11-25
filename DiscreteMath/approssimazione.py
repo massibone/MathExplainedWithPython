b2b6c324188238a1fe80589069542e37fc9026d6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-25 09:16:27 +0100

    Create approssimazione.py

---
+++ DiscreteMath/approssimazione.py
@@ -0,0 +1,11 @@
+'''
+Si consideri l’applicazione lineare f : M(3; R) → M(4; R) tale che
+f (0; 1; 1) = (3; 2; 0; 1)
+f (1; 0; 1) = (1; −1; 1; 0)
+f (2; 0; 0) = (0; 0; 2; 0):
+Allora la matrice A ∈ M(4; 3; R) associata alla base canonica tale che f = fA è
+0 2 1
+0 3 −1
+1 0 0
+0 1 0
+'''
