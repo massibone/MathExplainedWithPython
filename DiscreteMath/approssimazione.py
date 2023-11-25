643a76edb0f68eb6b46d76b1df7f289948840f2b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-25 09:17:00 +0100

    Update approssimazione.py

--- DiscreteMath/approssimazione.py
+++ DiscreteMath/approssimazione.py
@@ -9,3 +9,15 @@ Allora la matrice A ∈ M(4; 3; R) associata alla base canonica tale che f = fA 
 1 0 0
 0 1 0
 '''
+import numpy as np
+
+# Definire i vettori immagine f(e_i)
+f_e1 = np.array([0, 2, 1, 0])
+f_e2 = np.array([0, 3, -1, 1])
+f_e3 = np.array([1, 0, 0, 0])
+
+# Organizzare i vettori come colonne della matrice A
+A = np.column_stack((f_e1, f_e2, f_e3))
+
+print("La matrice A è:")
+print(A)
