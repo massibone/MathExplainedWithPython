8809ee0dc886dbee5d7d931d79c4a4293385d39a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-30 08:17:26 +0100

    Update pendoloSemplice.py

--- What_is_Mathematics/pendoloSemplice.py
+++ What_is_Mathematics/pendoloSemplice.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
+# Funzione che restituisce il sistema di equazioni differenziali del pendolo semplice
+def pendulum_system(y, t, g, L):
+    theta, omega = y
+    dydt = [omega, -g/L * np.sin(theta)]
+    return dydt
+
+# Parametri del pendolo
+g = 9.81  # accelerazione dovuta alla gravità (m/s^2)
+L = 1.0   # lunghezza del pendolo (m)
