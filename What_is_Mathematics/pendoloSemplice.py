e4d979ac9975a2dc9aefe25cdd6838027e575ec1
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-11-30 08:18:01 +0100

    Update pendoloSemplice.py

--- What_is_Mathematics/pendoloSemplice.py
+++ What_is_Mathematics/pendoloSemplice.py
@@ -11,3 +11,18 @@ def pendulum_system(y, t, g, L):
 # Parametri del pendolo
 g = 9.81  # accelerazione dovuta alla gravità (m/s^2)
 L = 1.0   # lunghezza del pendolo (m)
+
+# Condizioni iniziali
+theta0 = np.radians(45)  # angolo iniziale in radianti
+omega0 = 0.0             # velocità angolare iniziale
+
+# Array di tempo
+t = np.linspace(0, 10, 1000)  # intervallo di tempo da 0 a 10 secondi
+
+# Risolve il sistema di equazioni differenziali
+sol = odeint(pendulum_system, [theta0, omega0], t, args=(g, L))
+
+# Estrae l'angolo e la velocità angolare
+theta = sol[:, 0]
+omega = sol[:, 1]
+
