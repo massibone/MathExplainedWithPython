4fd9d106f98762859e38cf2813be914736ac88af
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-05 08:31:25 +0100

    Update Derivata.py

--- What_is_Mathematics/Derivata.py
+++ What_is_Mathematics/Derivata.py
@@ -1,1 +1,20 @@
 
+'''
+Quindi, la derivata della funzione radice quadra [(x+1)/(x-1)] è:
+
+f'(x) = 1/(2x-2)
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-2, 3, 100)
+
+y = np.sqrt(x+1) / (x-1)
+
+dy = 1 / (2*x-2)
+
+plt.plot(x, y, label="f(x)")
+plt.plot(x, dy, label="f'(x)")
+plt.legend()
+
+plt.show()
