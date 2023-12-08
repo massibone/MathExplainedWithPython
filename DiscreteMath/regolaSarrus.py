1613fb1ef1384eb1bcd8f31ffa9ddcb37225e95f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-08 08:34:37 +0100

    Update regolaSarrus.py

--- DiscreteMath/regolaSarrus.py
+++ DiscreteMath/regolaSarrus.py
@@ -12,3 +12,17 @@ det(A) = -12 - 4 + 0 + 3 + 8 + 0
 
 det(A) = -15
 '''
+
+import numpy as np
+
+# Definizione della nuova matrice
+B = np.array([[1, -2, -1],
+              [0, 3, 2],
+              [1, 1, -4]])
+
+# Calcolo del determinante di B
+det_B = np.linalg.det(B)
+
+# Stampa del risultato
+print(det_B)  # Output: -15.0
+
