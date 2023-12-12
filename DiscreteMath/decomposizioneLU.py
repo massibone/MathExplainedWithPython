d16e4f2c9f141d0bb7810eefd691a9b30a208a6a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-12 10:34:58 +0100

    Update decomposizioneLU.py

--- DiscreteMath/decomposizioneLU.py
+++ DiscreteMath/decomposizioneLU.py
@@ -1,1 +1,16 @@
 
+import numpy as np
+
+# Definisci la matrice A
+A = np.array([[1, 0, 3, 0],
+              [0, 1, 0, 3],
+              [2, 0, 1, 0],
+              [0, 2, 0, 1]])
+
+# Calcola la decomposizione LU di A
+P, L, U = scipy.linalg.lu(A)
+
+# Calcola l'inversa di A utilizzando la decomposizione LU
+A_inv = np.linalg.inv(U) @ np.linalg.inv(L) @ np.linalg.inv(P)
+
+print(A_inv)
