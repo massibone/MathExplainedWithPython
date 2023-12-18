ce8792bb98ed3195f6b1aa3251f0cd976d831b60
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-18 08:05:46 +0100

    Update orlate.py

--- DiscreteMath/orlate.py
+++ DiscreteMath/orlate.py
@@ -5,3 +5,19 @@ rispetto al vettore (2, 3, 1, 3),
 possiamo moltiplicare il vettore 
 per ogni riga di A
 '''
+import numpy as np
+
+# Definisci la matrice A
+A = np.array([[3, 1, 2],
+              [0, 1, 4],
+              [3, 3, -2],
+              [5, 0, 0],
+              [1, 1, -1]])
+
+# Definisci il vettore
+vettore = np.array([2, 3, 1, 3])
+
+# Calcola l'orlata di A rispetto al vettore
+orlata = A @ vettore
+
+print(orlata)
