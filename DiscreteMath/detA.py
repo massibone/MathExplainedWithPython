7e8269733829efbded9ff9dcc38057fcdee99f95
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-21 09:38:02 +0100

    calcolo determinante

--- DiscreteMath/detA.py
+++ DiscreteMath/detA.py
@@ -1,1 +1,13 @@
+import numpy as np
+
+# Definizione della matrice A
+A = np.array([[2, 1],
+              [-1, 3]])
+
+# Calcolo del determinante di A
+det_A = np.linalg.det(A)
+
+# Stampa del risultato
+print(det_A)  # Output: 7.0
+
 
