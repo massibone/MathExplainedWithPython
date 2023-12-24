c26f423f10dbc28d5e0137cac9f39b0cc3dce2d6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-24 16:34:14 +0100

    Update discrMath_Uguag2.py

--- DiscreteMath/discrMath_Uguag2.py
+++ DiscreteMath/discrMath_Uguag2.py
@@ -2,6 +2,7 @@
 soluzioni_x = []
 
 # Itera attraverso tutti i possibili valori di x nell'intervallo desiderato
+
 for x in range(-100, 101):  # Esempio: considera x da -100 a 100
     # Calcola le componenti delle due coppie ordinate
     componente1 = (x - 2, x + 1)
