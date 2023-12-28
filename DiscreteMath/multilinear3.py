4795a603cb4d4a19d04c699e5f94ea6bcec79496
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-28 09:47:59 +0100

    Update multilinear3.py

--- DiscreteMath/multilinear3.py
+++ DiscreteMath/multilinear3.py
@@ -25,4 +25,17 @@ def is_multilinear_symmetric(f):
         result1 = f(A, B)
         result2 = f(B, A)
         return np.array_equal(result1, result2)
+    # Esegui i test per verificare la linearità e la simmetria
+    A = np.array([[1, 2], [3, 4]])
+    B = np.array([[5, 6], [7, 8])
+    if check_first_variable(A, B) and check_second_variable(A, B) and check_symmetry(A, B):
+        return True
+    else:
+        return False
+
+# Esempio di applicazione
+def f(A, B):
+    result = A[0, 0]*B[0, 0] + A[0, 1]*B[0, 1] + A[1, 0]*B[1, 0] + A[1, 1]*B[1, 1]
+    return result
+
 
