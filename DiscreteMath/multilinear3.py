e19d75f1f9b1e734e162fbc56953ca98cf5f23d9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-12-28 09:46:49 +0100

    Update multilinear3.py

--- DiscreteMath/multilinear3.py
+++ DiscreteMath/multilinear3.py
@@ -2,3 +2,13 @@
 con = xz + yw   dovrebbe risultare multilineare e simmetrica
 '''
 
+import numpy as np
+
+def is_multilinear_symmetric(f):
+    # Verifica la linearità rispetto alla prima variabile
+    def check_first_variable(A, B):
+        alpha = 2
+        beta = 3
+        result1 = f(alpha * A + beta * A, B)
+        result2 = alpha * f(A, B) + beta * f(A, B)
+        return np.array_equal(result1, result2)
