42e8b2d5735462a4cb9a3565776c85f673d3e021
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-01 01:00:17 +0100

    Update minoreFondamentale.py

--- DiscreteMath/minoreFondamentale.py
+++ DiscreteMath/minoreFondamentale.py
@@ -6,3 +6,6 @@ A = np.array([[-2, 3, 1, -5],
               [0, 2, 2, -2],
               [-1, 3, 2, -4]])
 
+
+# Seleziona le righe e le colonne desiderate per il minore fondamentale
+sottomatrice = A[[1, 2], 2:4]
