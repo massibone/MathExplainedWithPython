318c0721c3b81522d7429989cc6130aceb0114d2
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-08 08:05:51 +0100

    Update risolvSistemaEquazioni2.py

--- DiscreteMath/risolvSistemaEquazioni2.py
+++ DiscreteMath/risolvSistemaEquazioni2.py
@@ -1,1 +1,22 @@
+'''
+dato il sistema
+2x1 − x2 + 6x4 = 2
+x1 + 2x2 − x3 + 2x4 = 1
+x2 − 2x3 + x4 = 1
+
+come si trovano le soluzioni? 
+
+'''
+import numpy as np
+
+# Definisci la matrice dei coefficienti e il vettore dei termini noti
+A = np.array([[2, -1, 0, 6], [1, 2, -1, 2], [0, 1, -2, 1]])
+B = np.array([2, 1, 1])
+
+# Risolvi il sistema di equazioni lineari
+soluzione = np.linalg.solve(A, B)
+
+# Stampa la soluzione
+print("La soluzione del sistema è:", soluzione)
+
 
