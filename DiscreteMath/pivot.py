e430bd4f231034b8c23877712142b58d993bd3c5
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-15 08:04:59 +0100

    Update pivot.py

--- DiscreteMath/pivot.py
+++ DiscreteMath/pivot.py
@@ -1,1 +1,26 @@
+import numpy as np
 
+# Creare una matrice 5x5 di esempio
+A = np.array([
+    [2, 1, 0, 3, 5],
+    [0, 0, 1, 2, 1],
+    [0, 0, 0, 1, 4],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+])
+
+# Eseguire la riduzione a forma normale di Gauss
+rref_A, pivot_columns = np.linalg.qr(A, mode='r')
+
+print("Matrice originale:\n", A)
+print("\nMatrice ridotta a forma normale di Gauss:\n", rref_A)
+print("\nColonne di pivot:", pivot_columns)
+'''
+RISULTATO:
+2 1 0 3 5
+0 0 1 2 1
+0 0 0 1 4
+0 0 0 0 0
+0 0 0 0 0
+Qui, i pivot sono gli elementi 2, 1, 1. I pivot sono nella colonna 1, 2 e 4.
+'''
