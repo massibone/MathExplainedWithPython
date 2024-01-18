f8b5a2ca68a3a02aba3fc0c6c72ea1b9e61cdf6f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-18 09:33:30 +0100

    Update volumeScatolaAperta.py

--- Mathematics/Analysis/volumeScatolaAperta.py
+++ Mathematics/Analysis/volumeScatolaAperta.py
@@ -2,3 +2,8 @@
 con un quadrato di lato a è stata costruita
 una scatolas rettangolare aperta di altezza x. il volume V della scatola è V=x(a-2x)^2
 '''
+def calcola_volume(a, x):
+    # Calcolo del volume della scatola rettangolare aperta
+    V = x * (a - 2 * x) ** 2
+    return V
+
