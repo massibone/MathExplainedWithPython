5bc72543c1e8ce38e40976492ea1a16cb8de13e2
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-18 09:34:12 +0100

    Update volumeScatolaAperta.py

--- Mathematics/Analysis/volumeScatolaAperta.py
+++ Mathematics/Analysis/volumeScatolaAperta.py
@@ -7,3 +7,12 @@ def calcola_volume(a, x):
     V = x * (a - 2 * x) ** 2
     return V
 
+# Input da parte dell'utente
+lato_quadrato = float(input("Inserisci la lunghezza del lato del quadrato (a): "))
+altezza_scatola = float(input("Inserisci l'altezza della scatola (x): "))
+
+# Calcola il volume utilizzando la funzione
+volume_scatola = calcola_volume(lato_quadrato, altezza_scatola)
+
+# Visualizza il risultato
+print(f"Il volume della scatola rettangolare aperta è: {volume_scatola}")
