61f917b8906e5db4dda55d3c25b22ca7f24c6589
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-20 07:37:25 +0100

    Update epimorfismo.py

--- DiscreteMath/epimorfismo.py
+++ DiscreteMath/epimorfismo.py
@@ -1,1 +1,12 @@
+'''
+Per verificare se una funzione 
+f:R ^3 →R ^2
+  è un epimorfismo, dovremmo verificare se la funzione copre l'intero codominio R^2 . In altre parole, ogni vettore 
+w∈R ^2
+  deve essere ottenibile come 
+
+f(v) per almeno un vettore v∈R^3
+
+'''
+
 
