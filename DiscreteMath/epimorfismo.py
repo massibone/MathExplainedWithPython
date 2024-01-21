4e10968eba02535525185238523101519230d237
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-21 07:27:38 +0100

    Update epimorfismo.py

--- DiscreteMath/epimorfismo.py
+++ DiscreteMath/epimorfismo.py
@@ -10,3 +10,15 @@ f(v) per almeno un vettore v∈R^3
 '''
 
 
+import numpy as np
+from scipy.optimize import fsolve
+
+def f(x, y, z):
+    return np.array([x + 2*y + z, y + z])
+
+def is_epimorphism(w):
+    # Funzione di sistema per risolvere per x, y, z
+    def system(vars):
+        x, y, z = vars
+        return [x + 2*y + z - w[0], y + z - w[1]]
+      
