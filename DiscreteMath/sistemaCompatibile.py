7d135f76518684da05e6b7aeca7f672d930df79c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-26 08:07:18 +0100

    Update sistemaCompatibile.py

--- DiscreteMath/sistemaCompatibile.py
+++ DiscreteMath/sistemaCompatibile.py
@@ -1,1 +1,12 @@
+'''
+Si consideri il sistema lineare
 
+x − y = 1
+x − y + z = 2
+−x + y + z = 0:
+Allora si ha che
+1. detta A la matrice incompleta, risulta rk(A) = 3,
+2. il sistema è compatibile,
+3. detta B la matrice completa, risulta rk(B) = 3,
+4. il sistema non è compatibile.
+v
