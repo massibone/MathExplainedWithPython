06255b36677295904717264208f1c70db27d0d56
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-27 13:52:47 +0100

    Update IntegraleIndefinito.py

--- Mathematics/Analysis/IntegraleIndefinito.py
+++ Mathematics/Analysis/IntegraleIndefinito.py
@@ -1,1 +1,5 @@
 
+'''
+In economia, l'integrale indefinito può essere utilizzato per calcolare il costo totale, il profitto o il surplus del consumatore in determinati modelli economici. Ad esempio, quando si analizzano i modelli di domanda e offerta, l'integrale indefinito può essere utilizzato per calcolare l'area sotto le curve di domanda e offerta per determinare il surplus del consumatore o del produttore.
+Per calcolare il surplus del consumatore, dobbiamo trovare l'area tra la curva di domanda e l'asse delle ordinate sopra il prezzo di equilibrio. Per fare ciò, dobbiamo prima trovare il prezzo di equilibrio risolvendo l'equazione di domanda e offerta.
+'''
