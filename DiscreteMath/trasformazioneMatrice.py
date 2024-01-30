5f2b3c388a09977c933cf7ca322bf4f860e79e74
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-30 08:37:22 +0100

    Update trasformazioneMatrice.py

--- DiscreteMath/trasformazioneMatrice.py
+++ DiscreteMath/trasformazioneMatrice.py
@@ -1,1 +1,5 @@
+'''
+ verificare se la matrice A2 può essere ottenuta dalla matrice A1 mediante una trasformazione lineare definita dalla matrice di trasformazione M ( (2,1, -1)
+'''
+import numpy as np
 
