2143b4fee64ea9b41c9161fec05e02fee6019fe3
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-01-30 08:37:54 +0100

    Update trasformazioneMatrice.py

--- DiscreteMath/trasformazioneMatrice.py
+++ DiscreteMath/trasformazioneMatrice.py
@@ -3,3 +3,18 @@
 '''
 import numpy as np
 
+# Definisci le matrici A1 e A2
+A1 = np.array([[1, 1, 3], [2, 1, 0], [0, -2, -1]])
+A2 = np.array([[1, 1, 3], [1, 0, -3], [0, -2, -1]])
+
+# Definisci la matrice di trasformazione M
+M = np.array([[2, 1, -1], [0, 1, 0], [0, 0, 1]])
+
+# Calcola il risultato della trasformazione di A1 tramite M
+result = np.dot(M, A1)
+
+# Verifica se il risultato è uguale a A2
+if np.array_equal(result, A2):
+    print("A2 è ottenuta da A1 tramite la trasformazione M.")
+else:
+    print("A2 non è ottenuta da A1 tramite la trasformazione M.")
