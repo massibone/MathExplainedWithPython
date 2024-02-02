3cc79e4a346968e20c2a76e5dc7899e04292b32f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-03 00:39:36 +0100

    Update FunzioniRazionali.py

--- Mathematics/Analysis/FunzioniRazionali.py
+++ Mathematics/Analysis/FunzioniRazionali.py
@@ -1,1 +1,4 @@
+'''
+Un esempio di applicazione dell'integrazione di funzioni razionali si trova nell'analisi di circuiti elettrici. In particolare, consideriamo un circuito RC (resistenza-capacità) in cui abbiamo un condensatore che si carica attraverso una resistenza.
+'''
 
