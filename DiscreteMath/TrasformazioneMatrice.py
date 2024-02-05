6477b1559b37331633e4f820df47edc107f6c5fb
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-05 08:37:53 +0100

    matrice A1 x trasformata M

--- DiscreteMath/TrasformazioneMatrice.py
+++ DiscreteMath/TrasformazioneMatrice.py
@@ -6,3 +6,14 @@ M⋅A1=A2
 
 In altre parole, moltiplicando la matrice A1 per la matrice di trasformazione M, otteniamo la matrice A2.
 '''
+import numpy as np
+
+# Definisci le matrici A1 e A2
+A1 = np.array([[1, 1, 3], [2, 1, 0], [0, -2, -1]])
+A2 = np.array([[1, 1, 3], [1, 0, -3], [0, -2, -1]])
+
+# Risolvi il sistema di equazioni lineari per ottenere M
+M = np.linalg.solve(A1, A2)
+
+print("Matrice di trasformazione M:")
+print(M)
