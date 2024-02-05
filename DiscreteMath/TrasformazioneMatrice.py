bea25155d30d31af018dc325afb952a63b8a461b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-05 08:37:09 +0100

    matrice A1 x trasformata M

--- DiscreteMath/TrasformazioneMatrice.py
+++ DiscreteMath/TrasformazioneMatrice.py
@@ -1,5 +1,6 @@
 '''
-La matrice di trasformazione M è ottenuta dall'operazione che trasforma la matrice A1 nella matrice A2. Per calcolare questa matrice di trasformazione, è possibile risolvere il sistema di equazioni lineari:
+La matrice di trasformazione M è ottenuta dall'operazione che trasforma la matrice A1 nella matrice A2. 
+Per calcolare questa matrice di trasformazione, è possibile risolvere il sistema di equazioni lineari:
 
 M⋅A1=A2
 
