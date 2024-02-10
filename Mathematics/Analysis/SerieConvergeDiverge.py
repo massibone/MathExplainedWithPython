874c7d2831856f2b3e29c038005f9584d15a293b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-10 09:19:23 +0100

    Update SerieConvergeDiverge.py

--- Mathematics/Analysis/SerieConvergeDiverge.py
+++ Mathematics/Analysis/SerieConvergeDiverge.py
@@ -1,1 +1,10 @@
 
+from sympy import symbols, limit, oo, sqrt
+
+# Esempio 1: Check se la sequenza converge o diverge
+x = symbols('x')
+r = limit((1 + 1/x)**x, x, oo)
+if r == 0:
+    print('La sequenza data non converge ad alcun numero')
+else:
+    print('La sequenza data converge ad un numero ' + str(r))
