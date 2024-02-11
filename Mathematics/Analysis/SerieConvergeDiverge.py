8fe380363a30ad5f77d2204b22ef35b31243f828
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-11 10:45:15 +0100

    Update SerieConvergeDiverge.py

--- Mathematics/Analysis/SerieConvergeDiverge.py
+++ Mathematics/Analysis/SerieConvergeDiverge.py
@@ -8,3 +8,19 @@ if r == 0:
     print('La sequenza data non converge ad alcun numero')
 else:
     print('La sequenza data converge ad un numero ' + str(r))
+# Esempio 2: Check se la sequenza converge o diverge
+x = symbols('x')
+r = limit((x)**2, x, oo)
+if r == oo:
+    print('La sequenza data non converge ad alcun numero')
+else:
+    print('La sequenza data converge ad un numero ' + str(r))
+
+# Esempio 3: Check se la sequenza converge o diverge
+x = symbols('x')
+r = limit(sqrt(x+1) - sqrt(x), x, oo)
+if r == oo:
+    print('La sequenza data non converge ad alcun numero')
+else:
+    print('La sequenza data converge ad un numero ' + str(r))
+    
