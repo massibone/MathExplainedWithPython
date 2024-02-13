04818f0f83517232fa55882f1bd9f8d82da3fafc
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-13 08:02:43 +0100

    Update dimScatolaLatta.py

--- DiscreteMath/dimScatolaLatta.py
+++ DiscreteMath/dimScatolaLatta.py
@@ -1,1 +1,3 @@
-
+'''
+dobbiamo trovare le dimensioni ottimali della scatola di latta (senza coperchio) che massimizzano il volume dato che si vuole utilizzare la minor quantità possibile di materiale.
+'''
