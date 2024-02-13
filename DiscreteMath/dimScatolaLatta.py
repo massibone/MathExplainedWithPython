c11e3273743109abcea65bd0e61949dfbaa76252
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-13 08:03:35 +0100

    Update dimScatolaLatta.py

--- DiscreteMath/dimScatolaLatta.py
+++ DiscreteMath/dimScatolaLatta.py
@@ -2,6 +2,7 @@
 dobbiamo trovare le dimensioni ottimali della scatola di latta (senza coperchio) 
 che massimizzano il volume dato che si vuole utilizzare la minor quantità possibile di materiale.
 '''
+
 from sympy import symbols, solve, diff, Eq
 
 # Definiamo le variabili simboliche
