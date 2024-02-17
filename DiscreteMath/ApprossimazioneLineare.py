f34359bfa6fa93e307dc660c4adb50cb055faeba
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-17 09:24:23 +0100

    Update ApprossimazioneLineare.py

--- DiscreteMath/ApprossimazioneLineare.py
+++ DiscreteMath/ApprossimazioneLineare.py
@@ -2,4 +2,6 @@
 L'approssimazione del costo totale di produzione intorno al livello di produzione 
 x=100 utilizzando la formula di Taylor di primo ordine.
 '''
-
+# Definiamo la funzione di costo totale
+def costo_totale(x):
+    return 1000 + 5*x + 0.1*x**2
