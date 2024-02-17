e99ed7a6b6d6ac230eece5a787745d8bd9107312
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-17 09:24:58 +0100

    Update ApprossimazioneLineare.py

--- DiscreteMath/ApprossimazioneLineare.py
+++ DiscreteMath/ApprossimazioneLineare.py
@@ -5,3 +5,7 @@ x=100 utilizzando la formula di Taylor di primo ordine.
 # Definiamo la funzione di costo totale
 def costo_totale(x):
     return 1000 + 5*x + 0.1*x**2
+    
+# Definiamo la funzione dell'approssimazione lineare
+def approssimazione_lineare(x, x0):
+    return 1050 + 5*(x - x0)
