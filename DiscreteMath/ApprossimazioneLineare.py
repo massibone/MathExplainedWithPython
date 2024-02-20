142f2d0f5be5afc3b53d90fd26ed107fc90e7296
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-20 16:44:00 +0100

    Update ApprossimazioneLineare.py

--- DiscreteMath/ApprossimazioneLineare.py
+++ DiscreteMath/ApprossimazioneLineare.py
@@ -9,3 +9,13 @@ def costo_totale(x):
 # Definiamo la funzione dell'approssimazione lineare
 def approssimazione_lineare(x, x0):
     return 1050 + 5*(x - x0)
+# Valore specificato di x intorno al quale vogliamo approssimare il costo totale
+x0 = 100
+
+# Calcoliamo il valore esatto di C(x) e della sua approssimazione lineare per x = 100
+C_x0 = costo_totale(x0)
+C_taylor_x0 = approssimazione_lineare(x0, x0)
+
+# Stampiamo i risultati
+print("Valore esatto di C(x) per x = 100:", C_x0)
+print("Approssimazione lineare di C(x) per x = 100:", C_taylor_x0)
