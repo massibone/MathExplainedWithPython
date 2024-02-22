918941a3a78eec26092e3441f9ac8c175d18125d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-22 08:01:43 +0100

    Update proiezione_prospettica.py

--- Mathematics/Analytical_Geometry/proiezione_prospettica.py
+++ Mathematics/Analytical_Geometry/proiezione_prospettica.py
@@ -1,1 +1,5 @@
-
+'''
+Questo esemio carica un'immagine, definisce i punti degli angoli dell'immagine originale e dell'immagine risultante (come se fosse vista dall'alto), 
+calcola la matrice di trasformazione prospettica e applica questa trasformazione all'immagine originale.
+Infine, visualizza sia l'immagine originale che l'immagine risultante utilizzando OpenCV.
+'''
