ab4336e46a921b72f59ca618a720eef54585bb6a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-23 08:09:27 +0100

    Update proiezione_prospettica.py

--- Mathematics/Analytical_Geometry/proiezione_prospettica.py
+++ Mathematics/Analytical_Geometry/proiezione_prospettica.py
@@ -3,3 +3,11 @@ Questo esemio carica un'immagine, definisce i punti degli angoli dell'immagine o
 calcola la matrice di trasformazione prospettica e applica questa trasformazione all'immagine originale.
 Infine, visualizza sia l'immagine originale che l'immagine risultante utilizzando OpenCV.
 '''
+import cv2
+import numpy as np
+
+# Carica l'immagine
+img = cv2.imread('image.jpg')
+
+# Definisci i punti dell'angolo della tua immagine originale
+points_A = np.float32([[320,15], [700,215], [85,610], [530,780]])
