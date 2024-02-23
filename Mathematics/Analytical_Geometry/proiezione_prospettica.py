6b6982010ae78077af5b4128656b45563cc6d61e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-23 08:09:48 +0100

    Update proiezione_prospettica.py

--- Mathematics/Analytical_Geometry/proiezione_prospettica.py
+++ Mathematics/Analytical_Geometry/proiezione_prospettica.py
@@ -11,3 +11,12 @@ img = cv2.imread('image.jpg')
 
 # Definisci i punti dell'angolo della tua immagine originale
 points_A = np.float32([[320,15], [700,215], [85,610], [530,780]])
+
+# Definisci i punti dell'angolo dell'immagine risultante (come se fosse vista dall'alto)
+points_B = np.float32([[0,0], [420,0], [0,594], [420,594]])
+
+# Calcola la matrice di trasformazione prospettica
+matrix = cv2.getPerspectiveTransform(points_A, points_B)
+
+# Applica la trasformazione prospettica all'immagine originale
+result = cv2.warpPerspective(img, matrix, (420, 594))
