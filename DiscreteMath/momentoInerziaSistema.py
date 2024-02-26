f7551f7ccfdf0e576212918c7368503a81bd1451
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-26 08:00:36 +0100

    Update momentoInerziaSistema.py

--- DiscreteMath/momentoInerziaSistema.py
+++ DiscreteMath/momentoInerziaSistema.py
@@ -1,7 +1,6 @@
 def momento_inerzia_sistema(masse, coordinate_punti, punto_p):
     M = 0
-    
-    for i in range(len(masse)):
+        for i in range(len(masse)):
         # Calcola la distanza tra il punto P e il punto i
         r = ((coordinate_punti[i][0] - punto_p[0])**2 +
              (coordinate_punti[i][1] - punto_p[1])**2 +
