a64b281877f1bbe129efcae16e7f30ed35d51e6f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-02-28 08:09:07 +0100

    Create minorTempo.py

---
+++ DiscreteMath/minorTempo.py
@@ -0,0 +1,3 @@
+'''
+un auto deve andare da A a B lungo il tratto AM e si muove con velocità v1 invece lungo MB con velocità v2.dove si troverebbe il punto M sulla retta DD' in modo che l'intero tratto AB sia percorso nel minor tempo possibile?
+'''
