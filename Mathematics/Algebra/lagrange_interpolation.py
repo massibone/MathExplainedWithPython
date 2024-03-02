62f269201ccf6229e9a7a9975a761abedba72c13
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-02 11:51:21 +0100

    Update lagrange_interpolation.py

--- Mathematics/Algebra/lagrange_interpolation.py
+++ Mathematics/Algebra/lagrange_interpolation.py
@@ -1,1 +1,5 @@
+'''
+Il contributo di Lagrange all'algebra è significativo e comprende diversi concetti e teoremi importanti. Uno di questi è il Teorema di Lagrange sull'Interpolazione Polinomiale, che afferma che dato un insieme di 
+n+1 punti distinti in un piano cartesiano, esiste esattamente un polinomio di grado n o inferiore che passa attraverso tutti questi punti.
+'''
 
