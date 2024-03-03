1f790f509d19f6c1c27d60a0833a36a0a82deb77
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-03 12:03:00 +0100

    Update lagrange_interpolation.py

--- Mathematics/Algebra/lagrange_interpolation.py
+++ Mathematics/Algebra/lagrange_interpolation.py
@@ -2,4 +2,18 @@
 Il contributo di Lagrange all'algebra è significativo e comprende diversi concetti e teoremi importanti. Uno di questi è il Teorema di Lagrange sull'Interpolazione Polinomiale, che afferma che dato un insieme di 
 n+1 punti distinti in un piano cartesiano, esiste esattamente un polinomio di grado n o inferiore che passa attraverso tutti questi punti.
 '''
+from sympy import symbols, simplify
 
+def lagrange_interpolation(points):
+    x = symbols('x')
+    polynomial = 0
+    n = len(points)
+
+    for i in range(n):
+        term = points[i][1]
+        for j in range(n):
+            if j != i:
+                term *= (x - points[j][0]) / (points[i][0] - points[j][0])
+        polynomial += term
+
+    return simplify(polynomial)
