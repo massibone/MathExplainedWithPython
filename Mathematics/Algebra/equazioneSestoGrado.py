b6fe7377f40a17a275007e9c43ff7d71c692ae08
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-05 08:01:37 +0100

    Update equazioneSestoGrado.py

--- Mathematics/Algebra/equazioneSestoGrado.py
+++ Mathematics/Algebra/equazioneSestoGrado.py
@@ -1,7 +1,13 @@
 '''
-Il contributo di Lagrange all'algebra include anche la sua investigazione sull'equazione generale di sesto grado, nota come l'equazione di sesto grado o l'equazione sestica. Tuttavia, risolvere l'equazione generale di sesto grado non è una procedura standard e non esiste una formula generale per le radici come nel caso di equazioni di grado inferiore.
+Il contributo di Lagrange all'algebra include anche la sua investigazione sull'equazione generale di sesto grado, 
+nota come l'equazione di sesto grado o l'equazione sestica.
+Tuttavia, risolvere l'equazione generale di sesto grado non è una procedura standard e non esiste una formula generale per le radici come nel caso di equazioni di grado inferiore.
 
-Lagrange ha sviluppato alcune tecniche per risolvere equazioni di sesto grado, ma queste tecniche possono essere complesse e richiedono l'uso di strumenti avanzati dell'analisi matematica. Una delle sue tecniche coinvolge l'uso della teoria dei gruppi e delle funzioni ellittiche.
+Lagrange ha sviluppato alcune tecniche per risolvere equazioni di sesto grado, ma queste tecniche possono essere complesse 
+e richiedono l'uso di strumenti avanzati dell'analisi matematica. Una delle sue tecniche coinvolge l'uso della teoria dei gruppi e delle funzioni ellittiche.
 
-In pratica, per risolvere equazioni di sesto grado, spesso ci si affida a metodi numerici o tecniche approssimate per trovare soluzioni approssimate o per risolvere casi specifici. Alcuni algoritmi numerici possono essere utilizzati per approssimare le radici di equazioni polinomiali di alto grado, ma non c'è una soluzione generale come nel caso delle equazioni di grado inferiore.
+In pratica, per risolvere equazioni di sesto grado, spesso ci si affida a metodi numerici 
+o tecniche approssimate per trovare soluzioni approssimate o per risolvere casi specifici. 
+Alcuni algoritmi numerici possono essere utilizzati per approssimare le radici di equazioni polinomiali di alto grado, 
+ma non c'è una soluzione generale come nel caso delle equazioni di grado inferiore.
 '''
