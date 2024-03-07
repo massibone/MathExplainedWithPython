1ebc57aec0ea720fd4105c7c45f8c490ac834539
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-07 08:02:55 +0100

    Create teoriaGalois.py

---
+++ Mathematics/Algebra/teoriaGalois.py
@@ -0,0 +1,10 @@
+'''
+La teoria dei gruppi di Galois, sviluppata da Évariste Galois nel 19° secolo, è un ramo dell'algebra astratta che studia le estensioni di campi e le loro corrispondenti simmetrie. È stata sviluppata in gran parte per rispondere alle domande sollevate dal teorema di Abel-Ruffini, che afferma l'impossibilità di risolvere equazioni polinomiali di grado cinque o superiore tramite radicali.
+
+La teoria di Galois fornisce un'importante struttura teorica per comprendere la solubilità di equazioni polinomiali e le loro soluzioni tramite formule esplicite. Utilizzando concetti come gruppi, campi, estensioni di campi e gruppi di Galois, questa teoria collega la struttura delle soluzioni di un'equazione polinomiale alla struttura dei suoi gruppi di Galois associati.
+
+La principale intuizione di Galois è stata quella di associare ogni estensione di campo ad un gruppo di Galois, che cattura le simmetrie dell'estensione e quindi dell'equazione polinomiale stessa. Questo collegamento tra gruppi di Galois e soluzioni di equazioni polinomiali ha portato a una comprensione più profonda della solubilità delle equazioni algebriche e delle loro proprietà.
+
+La teoria di Galois ha avuto un'influenza profonda su vari campi della matematica, tra cui la teoria dei numeri, l'analisi complessa, la geometria algebrica e la teoria dei codici. È uno dei concetti fondamentali dell'algebra moderna e continua a essere una parte essenziale dello studio delle equazioni polinomiali e delle loro soluzioni.
+'''
+
