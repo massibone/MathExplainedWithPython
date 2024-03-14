d0e71c922859c236498d083d162e71263e99de56
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-14 08:00:17 +0100

    Create problemaHurwitz.py

---
+++ Mathematics/Algebra/problemaHurwitz.py
@@ -0,0 +1,3 @@
+'''
+Certamente! Il problema di Hurwitz riguarda la stabilità di un polinomio, ed è solitamente esaminato per determinare se le radici di un polinomio sono tutte a parte reale negativa (o non positiva).
+'''
