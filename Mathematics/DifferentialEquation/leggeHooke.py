7de300186bec913de147fd295c626321da9aa522
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-16 10:53:54 +0100

    Update leggeHooke.py

--- Mathematics/DifferentialEquation/leggeHooke.py
+++ Mathematics/DifferentialEquation/leggeHooke.py
@@ -1,1 +1,4 @@
 
+'''
+La legge di Hooke descrive il comportamento di un materiale elastico sotto l'azione di una forza. In breve, essa stabilisce che lo spostamento (deformazione) di un materiale elastico è direttamente proporzionale alla forza applicata su di esso.
+'''
