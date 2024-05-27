e8ae0972523b3873d374a935fea9bdf035da365f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-27 08:07:58 +0200

    Create crittografiaAsimmetrica.py

---
+++ Commutative_Algebra/Moduli/crittografiaAsimmetrica.py
@@ -0,0 +1,8 @@
+'''
+In questo codice:
+
+Viene generata una coppia di chiavi RSA.
+Viene creato un messaggio di testo da cifrare.
+Il messaggio viene cifrato utilizzando la chiave pubblica.
+Il messaggio cifrato viene quindi decifrato utilizzando la chiave privata.
+'''
