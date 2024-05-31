2fce309fc7d745d2b74d8c11a4ecb065af8add5d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-31 13:54:24 +0200

    Update ideali.py

--- Commutative_Algebra/Anelli_e_Ideali/ideali.py
+++ Commutative_Algebra/Anelli_e_Ideali/ideali.py
@@ -1,9 +1,11 @@
 '''
-Gli ideali e gli anelli quozienti sono concetti fondamentali dell'algebra che possono essere compresi attraverso un esempio. Consideriamo l'anello degli interi Z e l'ideale I = 2Z, che rappresenta tutti i multipli di 2. 
+Gli ideali e gli anelli quozienti sono concetti fondamentali dell'algebra che possono essere compresi attraverso un esempio. 
+Consideriamo l'anello degli interi Z e l'ideale I = 2Z, che rappresenta tutti i multipli di 2. 
 
-- **Esempio di Ideale**: In questo caso, l'insieme I = {..., -4, -2, 0, 2, 4, ...} è un ideale di Z poiché è chiuso rispetto alla somma e al prodotto con qualsiasi elemento di Z. 
+**Esempio di Ideale**: In questo caso, l'insieme I = {..., -4, -2, 0, 2, 4, ...} è un ideale di Z poiché è chiuso rispetto alla somma e al prodotto con qualsiasi elemento di Z. 
 
-- **Anello Quoziente**: Costruiamo l'anello quoziente Z/2Z, che consiste nei resti della divisione per 2. Quindi, Z/2Z = {0 + 2Z, 1 + 2Z}. Questo anello ha due elementi: il resto 0 e il resto 1.
+**Anello Quoziente**: Costruiamo l'anello quoziente Z/2Z, che consiste nei resti della divisione per 2. 
+Quindi, Z/2Z = {0 + 2Z, 1 + 2Z}. Questo anello ha due elementi: il resto 0 e il resto 1.
 '''
 # Definizione dell'anello degli interi Z
 Z = set(range(-10, 11))  # Includiamo numeri da -10 a 10 per scopi illustrativi
