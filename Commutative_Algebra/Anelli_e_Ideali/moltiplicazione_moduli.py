822c0d2d5772920b67286126e05357a7c5a89eeb
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-02 07:35:56 +0200

    Update moltiplicazione_moduli.py

--- Commutative_Algebra/Anelli_e_Ideali/moltiplicazione_moduli.py
+++ Commutative_Algebra/Anelli_e_Ideali/moltiplicazione_moduli.py
@@ -1,1 +1,10 @@
+'''
+se nella scatola "Modulo 5" hai i numeri 1 e 3, e nella scatola "Modulo 3" hai i numeri 0 e 2, allora il prodotto di questi due sottomoduli sarebbe:
+
+1 * 0 = 0 (nel Modulo 5, 0 resta 0)
+1 * 2 = 2 (nel Modulo 5, 2 resta 2)
+3 * 0 = 0 (nel Modulo 5, 0 resta 0)
+3 * 2 = 1 (nel Modulo 5, 6 diventa 1)
+Quindi, il prodotto di questi due sottomoduli sarebbe una nuova scatola con gli elementi 0, 1 e 2, nel Modulo 5
+'''
 
