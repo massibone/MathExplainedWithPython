3965984a576d35abdf4a3e611b2e52c8b2cd60c6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-07 08:06:11 +0200

    Update funtore_Tor.py

--- Commutative_Algebra/Moduli/funtore_Tor.py
+++ Commutative_Algebra/Moduli/funtore_Tor.py
@@ -1,1 +1,8 @@
+'''
+Il funtore Tor è uno strumento potente in algebra omologica e teoria dei moduli. È utilizzato per misurare quanto una sequenza esatta di moduli fallisce nel rimanere esatta dopo aver applicato il prodotto tensoriale. In altre parole, il funtore Tor aiuta a capire le relazioni tra moduli e la loro struttura.
+Esempio Intuitivo
+Immagina di avere una catena di anelli metallici collegati tra loro, e vuoi vedere cosa succede se applichi una forza a questa catena. Se la catena si spezza in certi punti, il funtore Tor misura esattamente dove e come si spezza.
+
+In termini di moduli, immagina di avere una sequenza di moduli (insiemi strutturati di numeri o vettori) e vuoi vedere cosa succede se combini questi moduli con un altro modulo usando il prodotto tensoriale. Se la combinazione non si comporta come ti aspetti, il funtore Tor ti dice dove sta il problema.
+'''
 
