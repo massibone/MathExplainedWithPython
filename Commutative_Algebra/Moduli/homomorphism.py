b23552222217d84e10c64ed29f0a8b0599e2b51e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-09 08:13:17 +0200

    Update homomorphism.py

--- Commutative_Algebra/Moduli/homomorphism.py
+++ Commutative_Algebra/Moduli/homomorphism.py
@@ -1,1 +1,12 @@
+'''
+Immagina di avere tre scatole di mattoncini LEGO: A, B e C. Ogni scatola ha mattoncini speciali che possono essere collegati in modi specifici.
 
+Omomorfismo di anelli: Pensa a "f" e "g" come a due set di istruzioni su come collegare i mattoncini tra le scatole:
+"f" ti dice come passare i mattoncini dalla scatola A alla scatola B.
+"g" ti dice come passare i mattoncini dalla scatola B alla scatola C.
+Ora, immagina che:
+
+Se segui le istruzioni di "f" o "g", i mattoncini si collegano perfettamente e non c'è mai nessun problema (questa è la "piattezza").
+Se le istruzioni di "g" funzionano perfettamente per qualsiasi scatola con cui le provi (questa è la "fedeltà").
+Quello che vogliamo dimostrare è che se le istruzioni di "g" sono perfette e "f" o "g" sono buone istruzioni, allora le istruzioni di "f" devono essere buone.
+'''
