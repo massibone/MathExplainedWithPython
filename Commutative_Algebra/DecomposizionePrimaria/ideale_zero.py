5fc828f221856cf6e7ebf608a62ccdc529056775
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-15 11:13:59 +0200

    Update ideale_zero.py

--- Commutative_Algebra/DecomposizionePrimaria/ideale_zero.py
+++ Commutative_Algebra/DecomposizionePrimaria/ideale_zero.py
@@ -1,1 +1,12 @@
 
+'''
+L'ideale zero è come avere una scatola che contiene solo il numero zero. In matematica, l'ideale zero in un anello è l'insieme che contiene solo l'elemento zero.
+
+Divisori dello Zero
+Gli elementi divisori dello zero in un anello sono come mattoncini che, quando combinati con altri mattoncini, danno sempre zero. Ad esempio, se moltiplichi un mattoncino rosso con un mattoncino blu e ottieni zero, allora il mattoncino rosso o blu è un divisore dello zero.
+
+Ideali Primi e Decomposizione
+Un ideale primo è come una scatola di mattoncini che ha una proprietà molto speciale: ogni volta che combini due mattoncini e il risultato è nella scatola, allora almeno uno dei due mattoncini era già nella scatola.
+
+Quando diciamo che l'ideale zero è decomponibile, significa che possiamo trovare diversi ideali primi tali che, messi insieme, coprono tutti i divisori dello zero.
+'''
