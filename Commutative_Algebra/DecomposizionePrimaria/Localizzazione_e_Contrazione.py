35af57b03a6549fd6413ab9c7385c95e02dd307a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-16 00:50:30 +0200

    Update Localizzazione_e_Contrazione.py

--- Commutative_Algebra/DecomposizionePrimaria/Localizzazione_e_Contrazione.py
+++ Commutative_Algebra/DecomposizionePrimaria/Localizzazione_e_Contrazione.py
@@ -1,1 +1,8 @@
+'''
+Parte Moltiplicativa
+Una parte moltiplicativa è un gruppo speciale di mattoncini che, quando li usi per costruire, ti permettono di ottenere nuovi tipi di mattoncini. Questi nuovi tipi di mattoncini vengono chiamati localizzazione.
+
+Localizzazione e Contrazione
+Quando prendi un ideale e usi questi mattoncini moltiplicativi per ottenere nuovi tipi di mattoncini, ottieni qualcosa chiamato localizzazione dell'ideale. Se poi vuoi riportare questi nuovi mattoncini al loro stato originale nella scatola, fai una contrazione. La contrazione in A dell'ideale localizzato si denota con S(a).
+'''
 
