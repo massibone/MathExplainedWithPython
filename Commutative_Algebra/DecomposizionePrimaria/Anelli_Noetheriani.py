4dc4174e6c24b62649328cdabe96eafafb4125b0
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-18 12:15:31 +0200

    Update Anelli_Noetheriani.py

--- Commutative_Algebra/DecomposizionePrimaria/Anelli_Noetheriani.py
+++ Commutative_Algebra/DecomposizionePrimaria/Anelli_Noetheriani.py
@@ -1,1 +1,31 @@
+'''
+Cos'è un Ideale Primo?
+Un ideale primo è come una scatola speciale che ha una proprietà molto particolare: se prendi due mattoncini qualsiasi e il loro prodotto è dentro questa scatola, allora almeno uno dei due mattoncini era già nella scatola.
+
+Ideali Primari
+Gli ideali primari sono scatole che, pur essendo speciali, sono un po' meno rigide degli ideali primi. Quando moltiplichi due mattoncini e il risultato è dentro un ideale primario, uno dei mattoncini è quasi sempre dentro un ideale primo associato.
+
+Intersezione di Ideali Primari
+Quando prendi l'intersezione di tutti gli ideali primari associati a un ideale primo, ottieni un insieme che può essere rappresentato come 
+𝑆
+𝑝
+(
+0
+)
+S 
+p
+​
+ (0), dove 
+𝑆
+𝑝
+S 
+p
+​
+  è la parte moltiplicativa che esclude gli elementi dell'ideale primo 
+𝑝
+p.
+
+Anelli Noetheriani
+Gli anelli noetheriani sono come scatole di mattoncini che soddisfano certe proprietà di "finitezza". In questi anelli, la definizione che abbiamo visto si verifica sempre.
+'''
 
