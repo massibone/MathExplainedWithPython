9e0543022086a1c9d434f5bdf97b0f1996e9794e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-23 00:33:59 +0200

    Update going-down.py

--- Commutative_Algebra/ Dipendenza_Integrale/going-down.py
+++ Commutative_Algebra/ Dipendenza_Integrale/going-down.py
@@ -1,1 +1,3 @@
-
+'''
+Il teorema del Going-Down dice che se inizi con una costruzione (un ideale primo) in un livello superiore e vuoi scendere a un livello inferiore mantenendo certe regole speciali, puoi sempre trovare un modo per "scendere" senza perdere queste regole. In altre parole, se hai una sequenza di ideali primi e vuoi trovare un ideale primo in un livello inferiore che corrisponda a quello superiore, puoi farlo.
+'''
