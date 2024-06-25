956549930ee51862632ad8006b4a0b8ebe36f7d4
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-25 08:33:25 +0200

    Update domIntChiusi.py

--- Commutative_Algebra/ Dipendenza_Integrale/domIntChiusi.py
+++ Commutative_Algebra/ Dipendenza_Integrale/domIntChiusi.py
@@ -1,8 +1,13 @@
 '''
-domini di integrità integralmente chiusi in algebra commutativa.
-Un dominio di integrità è come una scatola di mattoncini LEGO in cui non puoi mai ottenere un prodotto di due mattoncini che sia zero, a meno che uno dei due mattoncini fosse già zero. Questo significa che puoi fare tutte le costruzioni che vuoi senza che improvvisamente qualcosa si disintegri o diventi inutile.
+Domini di integrità integralmente chiusi in algebra commutativa.
+Un dominio di integrità è come una scatola di mattoncini LEGO in cui non puoi mai ottenere un prodotto di due mattoncini che sia zero, 
+a meno che uno dei due mattoncini fosse già zero. 
+Questo significa che puoi fare tutte le costruzioni che vuoi senza che improvvisamente qualcosa si disintegri o diventi inutile.
 
-Un dominio di integrità è integralmente chiuso se ogni volta che prendi un mattoncino che "sembra" fatto dai mattoncini della tua collezione seguendo certe regole, è effettivamente un mattoncino della tua collezione. È come dire che se puoi costruire qualcosa usando i tuoi mattoncini secondo una regola speciale, allora quel qualcosa appartiene davvero alla tua collezione di mattoncini.
+Un dominio di integrità è integralmente chiuso se ogni volta che prendi un mattoncino che "sembra" fatto dai mattoncini della tua collezione seguendo certe regole, 
+è effettivamente un mattoncino della tua collezione. 
+È come dire che se puoi costruire qualcosa usando i tuoi mattoncini secondo una regola speciale, 
+allora quel qualcosa appartiene davvero alla tua collezione di mattoncini.
 '''
 
 
