a7aa9fe1f76073d59ceed8e369946e421335a545
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-26 08:49:18 +0200

    Update valutazione.py

--- Commutative_Algebra/ Dipendenza_Integrale/valutazione.py
+++ Commutative_Algebra/ Dipendenza_Integrale/valutazione.py
@@ -1,1 +1,7 @@
+'''
+Un anello di valutazione è come una scatola di mattoncini LEGO in cui puoi ordinare ogni costruzione in base a quanto è grande o piccola. C'è una funzione speciale, chiamata valutazione, che ti dice quanto è grande una costruzione rispetto ad altre.
+
+Un Esempio di Anello di Valutazione
+Pensa a un numero decimale, come 0.123. La valutazione ti dice quanti numeri ci sono dopo il punto decimale. Ad esempio, 0.123 ha una valutazione di 3 perché ci sono tre numeri dopo il punto decimale.
+'''
 
