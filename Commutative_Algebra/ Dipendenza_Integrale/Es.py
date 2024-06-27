fdcd9e92f340006f6e233a0210a9b3c4d31b992a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-27 08:35:07 +0200

    Update Es.py

--- Commutative_Algebra/ Dipendenza_Integrale/Es.py
+++ Commutative_Algebra/ Dipendenza_Integrale/Es.py
@@ -1,4 +1,8 @@
 '''
-sia A un dominio di integrità integralmente chiuso, K il suo capo delle frazioni e L un'estensione finita normale separabile K. Sia G il gruppo di Galois di L su K e sia B la chiusura integrale di A in  L. provare che sigma(B)=B per ogni sigma appartenente a G e inoltre che A =B^o
+sia A un dominio di integrità integralmente chiuso, K il suo capo delle frazioni e L un'estensione finita normale separabile K. 
+Sia G il gruppo di Galois di L su K e sia B la chiusura integrale di A in  L. provare che sigma(B)=B per ogni sigma appartenente a G e inoltre che A =B^o
 Cos'è un Dominio di Integrità Integralmente Chiuso?
-Immagina di avere una collezione di mattoncini LEGO che è perfetta: non ci sono mattoncini difettosi e ogni costruzione che fai è stabile e non si rompe mai. Questa collezione speciale è chiamata dominio di integrità integralmente chiuso.
+Immagina di avere una collezione di mattoncini LEGO che è perfetta: 
+non ci sono mattoncini difettosi e ogni costruzione che fai è stabile e non si rompe mai. 
+Questa collezione speciale è chiamata dominio di integrità integralmente chiuso.
+
