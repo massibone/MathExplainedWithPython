15f1c5c2cdd120dd8254368e4d87992791c11279
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-02 08:01:50 +0200

    Update LemmaNormalizzazioneNoether.py

--- Commutative_Algebra/ Dipendenza_Integrale/LemmaNormalizzazioneNoether.py
+++ Commutative_Algebra/ Dipendenza_Integrale/LemmaNormalizzazioneNoether.py
@@ -1,1 +1,10 @@
+'''
+Riassunto
+Dominio di Integrità: Collezione di mattoncini LEGO senza pezzi difettosi.
+Campo delle Frazioni: Possibilità di dividere i mattoncini in pezzi più piccoli.
+Estensione di Campo: Aggiungere nuovi tipi di mattoncini alla collezione.
+Gruppo di Galois: Maghi che fanno trasformazioni magiche sui mattoncini senza cambiare la collezione originale.
+Chiusura Integrale: Trovare tutti i mattoncini possibili creati combinando quelli originali senza pezzi difettosi.
+Lemma di Normalizzazione di Noether: Organizzare una collezione complessa di mattoncini usando pochi mattoncini di base per costruire tutto il resto.
+'''
 
