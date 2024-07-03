716ca601dacb15ccf669349fd1b119018a7ff108
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-03 08:01:11 +0200

    Update LemmaNormalizzazioneNoether.py

--- Commutative_Algebra/ Dipendenza_Integrale/LemmaNormalizzazioneNoether.py
+++ Commutative_Algebra/ Dipendenza_Integrale/LemmaNormalizzazioneNoether.py
@@ -9,3 +9,19 @@ Lemma di Normalizzazione di Noether:
 Organizzare una collezione complessa di mattoncini usando pochi mattoncini di base per costruire tutto il resto.
 '''
 
+import sympy as sp
+
+# Definiamo un anello di polinomi in tre variabili su Q
+R = sp.PolyRing('x y z', sp.QQ)
+x, y, z = R.gens
+
+# Definiamo un ideale generato da polinomi
+I = sp.Ideal(x**2 + y**2 - z**2, x**3 - y**3)
+
+# Troviamo i generatori dell'ideale
+generators = I.gens
+
+# Visualizziamo i risultati
+print("Anello R:", R)
+print("Ideale I:", I)
+print("Generatori dell'ideale I:", generators)
