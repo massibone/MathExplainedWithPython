001721b33e730605f77abf7c001e894f77881843
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-05 09:07:58 +0200

    Update TeoremaZeriHilbert.py

--- Commutative_Algebra/ Dipendenza_Integrale/TeoremaZeriHilbert.py
+++ Commutative_Algebra/ Dipendenza_Integrale/TeoremaZeriHilbert.py
@@ -1,1 +1,4 @@
 
+'''
+Il teorema degli zeri di Hilbert (nella sua forma debole) dice che se hai un insieme di equazioni in un campo algebricamente chiuso (come i numeri complessi), allora i punti che soddisfano queste equazioni sono esattamente i punti che puoi ottenere dall'ideale generato da queste equazioni.
+'''
