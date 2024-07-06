701f626e49b28e7785afa2e5b2d4d6ea08af153f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-07 01:03:25 +0300

    Update Ring_LWE.py

--- Commutative_Algebra/ Dipendenza_Integrale/Ring_LWE.py
+++ Commutative_Algebra/ Dipendenza_Integrale/Ring_LWE.py
@@ -1,5 +1,6 @@
 '''
-questo esempio, utilizzeremo la crittografia a chiave pubblica basata su Ring-LWE (Learning With Errors su anelli), una costruzione comune in crittografia moderna.
+questo esempio, utilizzeremo la crittografia a chiave pubblica basata su Ring-LWE (Learning With Errors su anelli),
+una costruzione comune in crittografia moderna.
 Obiettivo del Progetto
 Generazione delle chiavi: Creare una chiave pubblica e una chiave privata utilizzando un anello locale.
 Cifratura di un messaggio: Utilizzare la chiave pubblica per cifrare un messaggio.
