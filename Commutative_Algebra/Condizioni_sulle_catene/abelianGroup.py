c4e9349cbbaebbf3be586d66d4af371390f87832
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-12 08:11:30 +0200

    Update abelianGroup.py

--- Commutative_Algebra/Condizioni_sulle_catene/abelianGroup.py
+++ Commutative_Algebra/Condizioni_sulle_catene/abelianGroup.py
@@ -1,1 +1,16 @@
+'''
+ si potrebbe definire una classe AbelianGroup per rappresentare un gruppo abeliano finito e implementare metodi per eseguire operazioni di gruppo come la somma e la moltiplicazione.
+'''
+class AbelianGroup:
+  def __init__(self, elements, operation):
+    self.elements = elements
+    self.operation = operation
 
+  def __add__(self, other):
+    if not isinstance(other, AbelianGroup):
+      raise TypeError("Can only add AbelianGroup objects")
+    if self.elements != other.elements:
+      raise ValueError("Groups must have the same elements")
+    new_elements = [self.operation(a, b) for a in self.elements for b in other.elements]
+    return AbelianGroup(new_elements, self.operation)
+    
