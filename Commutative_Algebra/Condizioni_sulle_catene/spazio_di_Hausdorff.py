83d4285aba81cfad94eaf050d50a1fb688e0223d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-14 07:42:44 +0200

    Update spazio_di_Hausdorff.py

--- Commutative_Algebra/Condizioni_sulle_catene/spazio_di_Hausdorff.py
+++ Commutative_Algebra/Condizioni_sulle_catene/spazio_di_Hausdorff.py
@@ -9,3 +9,11 @@ class CompactHausdorffSpace:
         self.ax.set_xlim(0, size)
         self.ax.set_ylim(0, size)
         self.ax.set_aspect('equal')
+def plot_closed_set(self, closed_set, color='r'):
+        for (x, y, w, h) in closed_set:
+            rect = Rectangle((x, y), w, h, linewidth=1, edgecolor=color, facecolor='none')
+            self.ax.add_patch(rect)
+
+    def show(self):
+        plt.show()
+        
