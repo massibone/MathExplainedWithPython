fc81fadc225d3fa6a4362673cf1a2a396e5e650f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-14 07:40:19 +0200

    Update spazio_di_Hausdorff.py

--- Commutative_Algebra/Condizioni_sulle_catene/spazio_di_Hausdorff.py
+++ Commutative_Algebra/Condizioni_sulle_catene/spazio_di_Hausdorff.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+# Definizione di uno spazio compatto di Hausdorff X
+class CompactHausdorffSpace:
+    def __init__(self, size):
+        self.size = size
+        self.figure, self.ax = plt.subplots()
+        self.ax.set_xlim(0, size)
+        self.ax.set_ylim(0, size)
+        self.ax.set_aspect('equal')
