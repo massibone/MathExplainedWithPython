ac9791debb2edc108a493d47a5ce7bcfa739e22e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-17 08:06:05 +0200

    Update base_Hilbert.py

--- Commutative_Algebra/Anelli_Noetheriani/base_Hilbert.py
+++ Commutative_Algebra/Anelli_Noetheriani/base_Hilbert.py
@@ -1,1 +1,6 @@
+'''
+l teorema della base di Hilbert è un risultato fondamentale in algebra commutativa che afferma che se A è un anello noetheriano, allora l'anello dei polinomi A[x] è anch'esso noetheriano. In altre parole, ogni ideale di 
+A[x] è finitamente generato.
+'''
+
 
