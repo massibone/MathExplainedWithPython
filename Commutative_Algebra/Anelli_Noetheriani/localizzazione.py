8d2d1616595269fdea7e095a172df2a086065785
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-18 08:03:14 +0200

    Create localizzazione.py

---
+++ Commutative_Algebra/Anelli_Noetheriani/localizzazione.py
@@ -0,0 +1,3 @@
+'''
+Se 𝐴 è un anello noetheriano e S è una parte moltiplicativa arbitraria di A, allora l'anello localizzato S^(-1) A è anch'esso noetheriano. In altre parole, l'operazione di localizzazione preserva la proprietà noetheriana.
+'''
