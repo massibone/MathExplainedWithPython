444431ef9a8b27a706ae7d3d32796858e9020c18
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-23 08:04:25 +0200

    Update dominioDedekind.py

--- Commutative_Algebra/Domini_di_Dedekind/dominioDedekind.py
+++ Commutative_Algebra/Domini_di_Dedekind/dominioDedekind.py
@@ -1,1 +1,12 @@
+''''
+Un dominio di Dedekind è un dominio d'integrità (un tipo di anello commutativo senza divisori dello zero) con alcune proprietà particolarmente utili:
+
+Ogni ideale frazionario non nullo è invertibile: Questo significa che possiamo "annullare" un ideale frazionario in qualche senso preciso.
+
+Ogni ideale è un prodotto di ideali primi: Ogni ideale (insieme speciale di elementi) può essere scomposto in pezzi fondamentali (ideali primi), che sono come i mattoni di base per costruire altri ideali.
+
+È un anello noetheriano: Significa che non ci sono catene infinite di ideali crescenti, cioè possiamo sempre raggiungere un punto in cui non possiamo aggiungere ulteriori ideali.
+
+È integrally closed: Se un elemento nella frazione del dominio soddisfa un'equazione polinomiale con coefficienti nell'anello, allora l'elemento stesso appartiene all'anello.
+''''
 
