d961a2bfbbe96d3d80df8b0a3d04f3704536711b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-24 08:02:56 +0200

    Update localizzazione.py

--- Commutative_Algebra/Domini_di_Dedekind/localizzazione.py
+++ Commutative_Algebra/Domini_di_Dedekind/localizzazione.py
@@ -1,1 +1,10 @@
+'''
+Useremo un anello di Dedekind astratto e illustreremo la localizzazione
+'''
+import sympy as sp
+from sympy.abc import x
+
+# Definiamo un dominio di Dedekind astratto
+# In questo esempio usiamo il dominio degli interi algebrici in Q(sqrt(-5))
+A = sp.QQ.algebraic_field(x**2 + 5)
 
