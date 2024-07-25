4da3c2731dc7132d9ab73f4e5c3f9ad1983e3a52
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-25 08:29:29 +0200

    Update valutazioneDisceta.py

--- Commutative_Algebra/Domini_di_Dedekind/valutazioneDisceta.py
+++ Commutative_Algebra/Domini_di_Dedekind/valutazioneDisceta.py
@@ -1,1 +1,12 @@
+'''
+Un anello di valutazione discreta (DVR - Discrete Valuation Ring) è un tipo particolare di dominio di Dedekind con alcune proprietà specifiche.
+Proprietà di un Anello di Valutazione Discreta
+Dominio di integrità: Un DVR è un dominio di integrità, cioè un anello commutativo senza divisori dello zero.
+Valutazione discreta: Esiste una funzione di valutazione che assegna un valore intero a ciascun elemento, che riflette un "ordine" tra gli elementi.
+Unico ideale massimo: Ha un unico ideale massimo 
+𝑚
+m, generato da un elemento chiamato uniformizzante.
+Noetheriano: Ogni catena crescente di ideali si stabilizza, il che significa che è un anello Noetheriano.
+Fattorizzazione unica: Ogni elemento non nullo può essere scritto in modo unico (a meno di unità) come un prodotto di una potenza dell'uniformizzante e un'unità.
+'''
 
