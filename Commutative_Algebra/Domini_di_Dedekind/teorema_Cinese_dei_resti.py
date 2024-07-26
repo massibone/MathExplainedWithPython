33dd2ed1637fc198569daa104f9cc2fe2b91b876
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-26 08:17:06 +0200

    Update teorema_Cinese_dei_resti.py

--- Commutative_Algebra/Domini_di_Dedekind/teorema_Cinese_dei_resti.py
+++ Commutative_Algebra/Domini_di_Dedekind/teorema_Cinese_dei_resti.py
@@ -1,1 +1,12 @@
+from sympy import symbols, Eq, solve
+from sympy.ntheory.modular import crt
+
+# Definizione dei moduli e dei residui
+n = [3, 4, 5]
+a = [2, 3, 1]
+
+# Utilizzo del Teorema Cinese dei Resti
+x, mod = crt(n, a)
+
+print(f"Il valore di x che soddisfa tutte le congruenze è: {x} modulo {mod}")
 
