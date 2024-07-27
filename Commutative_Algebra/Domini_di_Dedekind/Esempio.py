2c8ca7d6b992af6a384ea33b4ac8f454946ac9af
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-27 09:02:36 +0200

    Create Esempio.py

---
+++ Commutative_Algebra/Domini_di_Dedekind/Esempio.py
@@ -0,0 +1,15 @@
+esempio.py
+''''
+Problema delle Luci Lampeggianti
+Immaginiamo che ci siano tre luci che lampeggiano a intervalli diversi. La prima luce lampeggia ogni 3 secondi, la seconda ogni 4 secondi e la terza ogni 5 secondi. Vogliamo sapere dopo quanti secondi tutte le luci lampeggeranno insieme per la prima volta.
+
+Descrizione del Problema
+Abbiamo tre intervalli:
+
+Luce 1: 3 secondi
+Luce 2: 4 secondi
+Luce 3: 5 secondi
+
+Il problema si riduce a trovare un valore di 
+𝑡 che soddisfa tutte le congruenze. Qui, i residui sono tutti zero, quindi possiamo usare il Teorema Cinese dei Resti direttamente.
+'''
