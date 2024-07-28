23e505955fc85c172dd2299fa31f597c4b77f22a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-07-28 10:39:06 +0200

    Update es2_CRT.py

--- Commutative_Algebra/Domini_di_Dedekind/es2_CRT.py
+++ Commutative_Algebra/Domini_di_Dedekind/es2_CRT.py
@@ -1,4 +1,4 @@
-''''
+'''
 Per pianificare operazioni periodiche in un sistema distribuito con diverse macchine o processi che operano a intervalli differenti, puoi utilizzare il Teorema Cinese dei Resti per sincronizzare i tempi di esecuzione. Questo può essere utile in scenari come il bilanciamento del carico, la sincronizzazione dei backup, o l'esecuzione di compiti periodici in un cluster eterogeneo.
 
 Step 1: Definire i Periodi delle Operazioni
@@ -9,3 +9,4 @@ Macchina Linux: 20 secondi
 Step 2: Calcolare il Tempo di Sincronizzazione
 Utilizzeremo il Teorema Cinese dei Resti per calcolare il tempo minimo 
 𝑡 dopo il quale tutte le operazioni saranno sincronizzate.
+'''
