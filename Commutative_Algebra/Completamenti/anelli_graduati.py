9d77d8e2a78a8b6d9296943407269245e7b209b6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-01 08:12:37 +0200

    Update anelli_graduati.py

--- Commutative_Algebra/Completamenti/anelli_graduati.py
+++ Commutative_Algebra/Completamenti/anelli_graduati.py
@@ -1,1 +1,4 @@
+'''
+Gli anelli graduati sono un concetto fondamentale in algebra commutativa e in geometria algebrica. Un anello graduato è un anello che è diviso in "pezzi" o "livelli" che possono essere studiati separatamente. 
+'''
 
