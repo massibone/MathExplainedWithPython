08e44d1cf57f126f9ac2154a08480a5aac105eb9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-01 08:13:12 +0200

    Update anelli_graduati.py

--- Commutative_Algebra/Completamenti/anelli_graduati.py
+++ Commutative_Algebra/Completamenti/anelli_graduati.py
@@ -17,3 +17,20 @@ class GraduatedRing:
         new_element = f"({element1} * {element2})"
         self.add_element(new_degree, new_element)
         return new_degree, new_element
+
+
+    def display(self):
+        for degree in sorted(self.elements.keys()):
+            print(f"Degree {degree}: {self.elements[degree]}")
+
+# Creazione di un anello graduato
+graduated_ring = GraduatedRing()
+graduated_ring.add_element(0, "1")      # elemento neutro
+graduated_ring.add_element(1, "x")      # elemento di grado 1
+graduated_ring.add_element(2, "x^2")    # elemento di grado 2
+
+# Moltiplicazione di elementi
+degree, element = graduated_ring.multiply(1, "x", 1, "x")
+
+# Visualizzazione dell'anello graduato
+graduated_ring.display()
