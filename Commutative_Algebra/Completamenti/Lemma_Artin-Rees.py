1f7d979ea198ab8ba20f1cbfa3da66b9d5965e0c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-03 00:55:58 +0200

    Update Lemma_Artin-Rees.py

--- Commutative_Algebra/Completamenti/Lemma_Artin-Rees.py
+++ Commutative_Algebra/Completamenti/Lemma_Artin-Rees.py
@@ -1,1 +1,3 @@
-
+''
+Il lemma di Artin-Rees dice che, se tagli la pila grande in pezzi sempre più piccoli con il coltello 𝐼, a un certo punto la pila piccola 𝑁 si comporta in modo prevedibile rispetto ai pezzi della pila grande.
+''
