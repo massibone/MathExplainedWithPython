97fe1d59d97a98e75b114916e568ee6bb798a654
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-07 08:09:37 +0200

    Update topologia_a-adica.py

--- Commutative_Algebra/Completamenti/topologia_a-adica.py
+++ Commutative_Algebra/Completamenti/topologia_a-adica.py
@@ -1,3 +1,5 @@
 '''
-La topologia a-adica è una struttura fondamentale nell'algebra commutativa che permette di studiare la convergenza di sequenze e serie in un contesto algebrico. Viene utilizzata per definire una nozione di "vicinanza" tra elementi di un anello o modulo, utilizzando un ideale 𝑎 come base per la topologia.
+La topologia a-adica è una struttura fondamentale nell'algebra commutativa che permette di studiare 
+la convergenza di sequenze e serie in un contesto algebrico. Viene utilizzata per definire 
+una nozione di "vicinanza" tra elementi di un anello o modulo, utilizzando un ideale 𝑎 come base per la topologia.
 '''
