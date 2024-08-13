62e64094809c6c6674430840de9c49d1dc8086ab
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-13 08:04:33 +0200

    Update funzioneHilbert.py

--- Commutative_Algebra/Teoria_della_dimensione/funzioneHilbert.py
+++ Commutative_Algebra/Teoria_della_dimensione/funzioneHilbert.py
@@ -1,1 +1,4 @@
-
+'''
+Polinomio di Hilbert
+Per moduli graduati su anelli di polinomi, la funzione di Hilbert è spesso un polinomio, noto come polinomio di Hilbert. Questo polinomio cattura il comportamento asintotico della funzione di Hilbert.
+'''
