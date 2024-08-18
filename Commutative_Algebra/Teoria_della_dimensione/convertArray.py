60d85dfe887271c2f1a15fd50916a456c5f132f3
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-18 09:13:19 +0200

    Update convertArray.py

--- Commutative_Algebra/Teoria_della_dimensione/convertArray.py
+++ Commutative_Algebra/Teoria_della_dimensione/convertArray.py
@@ -1,1 +1,4 @@
-convertArray.py
+
+'''
+conversione di un'immagine in un array di numeri con caratteristiche di Hilbert richiede diverse librerie e passaggi dettagliati. Per fornirti un esempio utile e contestuale, avrei bisogno di informazioni specifiche sulle caratteristiche di Hilbert che ti interessano (ad esempio, intensità, texture, Carr) e sul formato delle immagini (ad esempio, JPG, PNG).
+'''
