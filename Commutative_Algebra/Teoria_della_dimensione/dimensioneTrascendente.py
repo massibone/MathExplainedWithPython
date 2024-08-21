3a4cce613889a33840d7b2d60a24ab35d1cc4fb4
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-21 08:03:18 +0200

    Update dimensioneTrascendente.py

--- Commutative_Algebra/Teoria_della_dimensione/dimensioneTrascendente.py
+++ Commutative_Algebra/Teoria_della_dimensione/dimensioneTrascendente.py
@@ -1,1 +1,3 @@
-
+'''
+La dimensione trascendente è un concetto fondamentale nell'algebra commutativa e nella teoria dei campi, che misura l'estensione di un campo rispetto a un suo sottocampo. È strettamente legata alla nozione di indipendenza algebrica.
+'''
