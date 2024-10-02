c33e4159214efc117a08cf195b5112a668b69d60
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-02 08:20:47 +0200

    Update DecomposizioneCholesky.py

--- Algebra/DecomposizioneLineare/DecomposizioneCholesky.py
+++ Algebra/DecomposizioneLineare/DecomposizioneCholesky.py
@@ -1,1 +1,10 @@
+# Creiamo una matrice definita positiva (necessaria per la decomposizione di Cholesky)
+A_cholesky = np.array([[4, 2], [2, 3]])
 
+# Decomposizione di Cholesky
+L = np.linalg.cholesky(A_cholesky)
+
+print("Matrice triangolare inferiore L:")
+print(L)
+print("Trasposta di L (L^T):")
+print(L.T)
