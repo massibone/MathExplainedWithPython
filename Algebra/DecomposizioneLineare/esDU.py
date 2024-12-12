81338ce18e414b9c94c83c18a06882b4b68284a7
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-12 08:07:35 +0100

    Update esDU.py

--- Algebra/DecomposizioneLineare/esDU.py
+++ Algebra/DecomposizioneLineare/esDU.py
@@ -1,1 +1,4 @@
-
+'''
+Decomposizione QR: Applicazioni nella regressione lineare
+Nell'analisi statistica e nelle previsioni economiche, la regressione lineare viene utilizzata per trovare la relazione tra variabili. Durante la risoluzione del problema di minimizzazione dei quadrati (es. fitting di una retta ai dati), la decomposizione QR è una tecnica efficiente per trovare una soluzione.
+'''
